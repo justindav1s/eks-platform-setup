13d740329d5d103f0a409a60c8cdff60b60a4b9e
Justin Davis <justinndavis@gmail.com>
2021-06-02 11:06:07 +0100

    flux2 updates

--- bin/flux2/deploy_flux2.sh
+++ bin/flux2/deploy_flux2.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+#setup your gitlab acess token first as shell var : GITLAB_TOKEN
+
+
+CLUSTER=eks-dev1
+REPO=flux2-ops
+GITLAB_USER=justinndavis
+
+kubectl create namespace flux-system
+
+flux bootstrap gitlab \
+  --owner=$GITLAB_USER \
+  --repository=$REPO \
+  --branch=master \
+  --path=./clusters/$CLUSTER \
+  --personal
