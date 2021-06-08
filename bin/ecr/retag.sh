91e933091ad58bf75ec44b6f64a915a38c2c8629
Justin Davis <justinndavis@gmail.com>
2021-06-08 12:23:50 +0100

    updates

---
+++ bin/ecr/retag.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+IMAGE=inventory
+OLD_VERSION=latest
+NEW_VERSION=1.0.3
+REGISTRY_HOST=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+
+OLD_TAG=$REGISTRY_HOST/$IMAGE:$OLD_VERSION
+NEW_TAG=$REGISTRY_HOST/$IMAGE:$NEW_VERSION
+
+export DOCKER_PASSWORD=$(aws ecr get-login-password --region ${AWS_REGION})
+
+echo $DOCKER_PASSWORD
+
+docker login -u AWS -p $DOCKER_PASSWORD https://${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+docker pull $OLD_TAG
+
+docker tag $OLD_TAG $NEW_TAG
+
+docker push $NEW_TAG
