45d879d14fd59120df92c860880c8717d6d85a9a
Justin Davis <justinndavis@gmail.com>
2021-06-09 15:06:32 +0100

    flux2 uninstall

--- flux2/remove_flux2.sh
+++ flux2/remove_flux2.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-export KUBECONFIG=~/.kube/eksdev1.config
 
-export TOKEN=$(kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}")
-export SERVER=https://EFA94DF9F7D2E0E18D12ED4DF843AF82.gr7.eu-west-2.eks.amazonaws.com
+flux uninstall --namespace=flux-system
 
-kubectl get kustomization flux-system -n flux-system -o json > kustomization.json
+# export KUBECONFIG=~/.kube/eksdev1.config
 
-curl -k -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TOKEN" \
-    -X PUT \
-    --data-binary @kustomization.json \
-    $SERVER/apis/kustomize.toolkit.fluxcd.io/v1beta1/namespaces/flux-system/kustomizations/flux-system
+# export TOKEN=$(kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}")
+# export SERVER=https://EFA94DF9F7D2E0E18D12ED4DF843AF82.gr7.eu-west-2.eks.amazonaws.com
 
-kubectl get namespace flux-system -o json > flux-system-ns.json  
+# kubectl get kustomization flux-system -n flux-system -o json > kustomization.json
 
-echo sleeping .....
-sleep 60
+# curl -k -H "Content-Type: application/json" \
+#     -H "Authorization: Bearer $TOKEN" \
+#     -X PUT \
+#     --data-binary @kustomization.json \
+#     $SERVER/apis/kustomize.toolkit.fluxcd.io/v1beta1/namespaces/flux-system/kustomizations/flux-system
 
-curl -k -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TOKEN" \
-    -X PUT \
-    --data-binary @flux-system-ns.json  \
-    $SERVER/api/v1/namespaces/flux-system/finalize
+# kubectl get namespace flux-system -o json > flux-system-ns.json  
+
+# echo sleeping .....
+# sleep 60
+
+# curl -k -H "Content-Type: application/json" \
+#     -H "Authorization: Bearer $TOKEN" \
+#     -X PUT \
+#     --data-binary @flux-system-ns.json  \
+#     $SERVER/api/v1/namespaces/flux-system/finalize
