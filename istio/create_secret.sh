a4731c9427b1f41e5183578ab8d683d7b4aee3d2
Justin Davis <justinndavis@gmail.com>
2021-06-21 13:46:25 +0100

    fixes

--- istio/create_secret.sh
+++ istio/create_secret.sh
@@ -3,7 +3,9 @@
 KEY=/Users/justin/acme/certs/openshiftlabs.net/openshiftlabs.net.key
 CERT=/Users/justin/acme/certs/openshiftlabs.net/openshiftlabs.net.cer
 
-kubectl delete -n istio-system secret kong-tls-credentials 
+kubectl delete -n default secret tls-keys 
+kubectl delete -n istio-system secret tls-keys
 
-kubectl create -n istio-system secret tls kong-tls-credentials --key=${KEY} --cert=${CERT}
+kubectl create -n default secret tls tls-keys --key=${KEY} --cert=${CERT}
+kubectl create -n istio-system secret tls tls-keys --key=${KEY} --cert=${CERT}
 
